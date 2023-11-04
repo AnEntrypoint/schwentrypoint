5dab40aa694417f7bf3946ac6f0666c90cde32d9
lanmower <almagestfraternite@gmail.com>
2023-11-04 17:09:41 +0200

    moar

--- player_input.gd
+++ player_input.gd
@@ -14,7 +14,7 @@ extends MultiplayerSynchronizer
 
 @export_enum("ThirdPerson", "FirstPerson") var cameraType:int = 0
 @export var is_on_floor = false
-
+var wannajump = 0
 @export var input_dir = Vector3()
 
 func _ready():
@@ -22,10 +22,18 @@ func _ready():
 	set_process(get_multiplayer_authority() == multiplayer.get_unique_id())
 
 func _process(_delta):
+	if wannajump:
+		wannajump += _delta
+	if Input.is_action_just_pressed("jump"):
+		wannajump = 0.0001
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
+	if wannajump < 0.3 and wannajump != 0 and is_on_floor and !crouch:
+		jumping = true
+		wannajump = 0
 	if Input.is_action_just_pressed("jump") and is_on_floor and !crouch:
 		jumping = true
+		wannajump = 0
 	if Input.is_action_just_pressed("Crouch"):
 		crouch = true
 	if Input.is_action_just_pressed("camSwitch"):
